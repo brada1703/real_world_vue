2cd2240bf5fbf75691bb08114e21ea3dbb8dc68f
Brad Ahrens <bradley.ahrens@kwanko.com>
2020-05-09 23:28:46 +0100

    chore: add Vue Router basics

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,26 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import EventCreate from "../views/EventCreate.vue";
+import EventList from "../views/EventList.vue";
+import EventShow from "../views/EventShow.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "event-list",
+    component: EventList
   },
   {
-    path: "/about",
-    name: "About",
-    alias: "/test",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/event-show",
+    name: "event-show",
+    component: EventShow
   },
   {
-    path: "/about-us",
-    redirect: {
-      name: "About"
-    }
+    path: "/event-create",
+    name: "event-create",
+    component: EventCreate
   }
 ];
 
